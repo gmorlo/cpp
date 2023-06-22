596a130c4b12559922930f1176aeabc61c105e72
gmorlo <137416617+gmorlo@users.noreply.github.com>
2023-06-22 16:36:05 +0200

    Update main.cpp

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <cstdlib>
 
 int main() {
-    // Write C++ code here
-    std::cout << "Hello world!";
+    const char array[][10] = {"placek", "kokos", "poziomka", "arbuz"};
 
+    int array_size = sizeof(array) / sizeof(array[0]);
+    
+    int randomIndex = rand() % array_size;
+    
+    const char* variable = array[randomIndex];
+
+    std::cout << variable;
     return 0;
 }
