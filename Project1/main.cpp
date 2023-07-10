c7ebd9e969c569af1538b1b6c067d445359d3913
gmorlo <137416617+gmorlo@users.noreply.github.com>
2023-07-10 11:39:00 +0200

    Update main.cpp

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,27 +1,57 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <string>
+
+class RandomArray {
+private:
+    int size;
+    std::string* array;
+
+public:
+    RandomArray(int size) {
+        this->size = size;
+        this->array = new std::string[size];
+    }
+
+    ~RandomArray() {
+        delete[] array;
+    }
+
+    void readElements() {
+        std::string input;
+        for (int i = 0; i < size; i++) {
+            std::cout << "Podaj " << i + 1 << " element: ";
+            std::cin >> input;
+            array[i] = input;
+        }
+    }
+
+    void printRandomElement() {
+        srand(time(0));
+        int randomIndex = rand() % size;
+        std::string variable = array[randomIndex];
+        std::cout << "Losowy element: " << variable << std::endl;
+    }
+
+    void printAllElements() {
+        std::cout << "Wszystkie elementy: ";
+        for (int i = 0; i < size; i++) {
+            std::cout << array[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+};
 
 int main() {
     int x = 0;
     std::cout << "Podaj rozmiar tablicy: ";
     std::cin >> x;
-    char array[x] = {}; // Zakładam maksymalną długość łańcucha jako 100
-    char b;
-    
-    for (int i = 0; i < x; i++) {
-        std::cout << "Podaj " << i + 1 << " element: ";
-        std::cin >> array[i];
-    }
-    
-    int array_size = sizeof(array) / sizeof(array[0]);
-    
-    srand(time(0));  // Inicjalizacja generatora liczb losowych na podstawie aktualnego czasu
-    
-    int randomIndex = rand() % array_size;
-    
-    char variable = array[randomIndex];
 
-    std::cout << variable;
+    RandomArray randomArray(x);
+    randomArray.readElements();
+    randomArray.printRandomElement();
+    randomArray.printAllElements();
+
     return 0;
 }
