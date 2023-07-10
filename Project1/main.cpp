f383dab3e5da50e43ae2d7cd50eee80e36e2de16
gmorlo <137416617+gmorlo@users.noreply.github.com>
2023-07-10 11:19:12 +0200

    Update main.cpp

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 
 int main() {
-    const char array[][10] = {"placek", "kokos", "poziomka", "arbuz"};
-
+    int x = 0;
+    std::cout << "Podaj rozmiar tablicy: ";
+    std::cin >> x;
+    char array[x] = {}; // Zakładam maksymalną długość łańcucha jako 100
+    char b;
+    
+    for (int i = 0; i < x; i++) {
+        std::cout << "Podaj " << i + 1 << " element: ";
+        std::cin >> array[i];
+    }
+    
     int array_size = sizeof(array) / sizeof(array[0]);
+    
+    srand(time(0));  // Inicjalizacja generatora liczb losowych na podstawie aktualnego czasu
     
     int randomIndex = rand() % array_size;
     
-    const char* variable = array[randomIndex];
+    char variable = array[randomIndex];
 
     std::cout << variable;
     return 0;
